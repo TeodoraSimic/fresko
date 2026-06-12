34abf6b42ae187ad2c2f37632144c8cee5f99079
Toedora <Toedora@DESKTOP-SEVT0CA>
2026-06-12 21:20:52 +0200

    Prikaz proizvoda sa slikama na početnoj strani

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,19 @@
+import { Row, Col } from 'react-bootstrap'
+import products from '../products_list'
+import Product from '../components/Product'
+
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>Dobrodošli u FRESHKO</h1>
-      <p>Sveže voće i povrće</p>
-    </div>
+    <>
+      <h1>Najnovije voće i povrće</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
